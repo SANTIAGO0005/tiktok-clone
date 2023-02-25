34653cb643b0682be97c034eb4811239eeada32d
Santiago Cifuentes <cifuentess938@gmail.com>
2023-02-25 13:12:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <main>
+        Tik Tok Clone
+      </main>
+      
+    </div>
+  )
+}
+
+export default App
